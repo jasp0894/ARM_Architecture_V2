6dddfbf99b2bf82cec894a8a1add06b9bc5db69c
Unknown <jesus.sanchez1@upr.edu>
2017-05-19 01:34:03 -0400

    Control Unit is finally working!!!!!!!!!1
    A few addressing modes has been successfully tested

--- controlUnit_piecewise_Tester.v
+++ controlUnit_piecewise_Tester.v
@@ -11,7 +11,7 @@ module CU_Tester;
 //63 62 61 60 59 58      57-55  54 53 52-50   49-42    41-34  33    32   31   30     29  28  27  26  25  24  23  22  21  20  19  18  17 16  15  14  13  12  11      10  9  8    7     6      5       4       3     2   1  0
 //                       N2-N0 INV MI S2-S0 CR15-CR8 CR7-CR0 FRLd RFLd IRLd MARLd MDRLd R/W MOV MA1 MA0 MB2 MB1 MB0 MC2 MC1 MC0 MD1 MD0 ME OP4 OP3 OP2 OP1 OP0 SLS_EN MS2 MS1 MS0 LSM_EN LSM_IN2 LSM_IN1 LSM_IN0 MH1 MH0 MF
 
-	parameter sim_time = 100000;
+	parameter sim_time = 1000;
 
 	//module instantiation
 	controlUnit_p cu (CTL,IR,MOC,COND,LSM_DETECT,LSM_END,CLK);
@@ -21,17 +21,17 @@ module CU_Tester;
 		begin
 			//IRPUTS initialization
 
-			MOC=1'd0; COND=1'd0; LSM_DETECT=1'd0; LSM_END=1'd0; CLK=1'd0; 			
+			MOC=1'd1; COND=1'd0; LSM_DETECT=1'd0; LSM_END=1'd0; CLK=1'd0; 			
 
 			
 
 			//0	0	0	0	1	0	1	L	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0	0
 
 
-			IR = 32'b00001011000000000000000000000000; //44
+			//IR = 32'b00001011000000000000000000000000; //44
 
 
-			  //IR = 32'b00001000000000000000000000000000; //30
+			IR = 32'b00001000000000000000000000000000; //30
 
 
 
@@ -42,7 +42,7 @@ module CU_Tester;
 
 			//IR = 32'b00000111101100000000000000000000; //23
 
-			//IR = 32'b00000101111100000000000000000000; //19
+			//IR = 32'b00000101111000000000000000000000; //19
 
 
 
