55600b5f931f29b14ae1cf342f46240d10378eb1
Unknown <pepo023@outlook.com>
2017-06-11 14:26:13 -0400

    MUX 4bit

---
+++ mux_8x1_4bit.v
@@ -0,0 +1,26 @@
+//---------------------Mux 8x1 1bit----------------------
+module mux_8x1 (Y, S, A, B, C, D, E, F, G, H);
+	
+	//Inputs
+	input wire [3:0] A,B,C,D,E,F,G,H;
+	input wire [2:0] S;
+
+	//Outputs
+	output wire [3:0] Y;
+
+	//Test for selection bit
+	always @ (S,A,B,C,D,E,F,G,H)
+
+		case(S)
+			3'b000:	Y=A;
+			3'b001:	Y=B;
+			3'b010:	Y=C;
+			3'b011:	Y=D;
+			3'b100:	Y=E;
+			3'b101:	Y=F;
+			3'b110:	Y=G;
+			3'b111:	Y=H;
+
+		endcase // S
+
+endmodule // mux_8x1
