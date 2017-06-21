61f9b7ddbd174acb8b50d629fe8395ce53cc7af9
Unknown <pepo023@outlook.com>
2017-06-20 18:48:57 -0700

    CU working
    check branch states. I think a Branch & link instruction is being interpreted as a branch instruction only.

--- ControlUnit_Tester_pepo.v
+++ ControlUnit_Tester_pepo.v
@@ -6,39 +6,58 @@ module ControlUnit_Tester_pepo;
 	reg COND;
 	reg LSM_DETECT;
 	reg LSM_END;
+	reg RESET;
 	reg CLK;
 
 	//output signals
 	wire [33:0] cu_datapath;
 
 	//Simulatio Parameters
-	parameter sim_time = 50000;
+	parameter sim_time = 40;
 
 	//module instantiation
-	cu_pepo cu_pepo (IR, MOC, COND, LSM_DETECT, LSM_END, CLK, cu_datapath);
+	cu_pepo cu_pepo (IR, MOC, COND, LSM_DETECT, LSM_END, RESET, CLK, cu_datapath);
 
 	initial
 		begin
 			//INPUTS initialization
-	
-			IR = 32'b11100101110101000101000000000100; //State 16
-			MOC = 1'd0; COND=1'd0; MLS0=1'd0; MLS1=1'd0; CLK=1'd0;
+			fork
+			MOC = 1'b0;
+			COND = 1'b0;
+			LSM_DETECT = 1'b0;
+			LSM_END = 1'b0;
+			RESET = 1'b0;
+			CLK = 1'b0;
+			join
 
+			#1 RESET = 1'b1;
+			#2 RESET = 1'b0;
 
-			#200;
+			IR = 32'b00011010111111111111111111111101; //Load/Store Register Offset
+			MOC = 1'd1; COND=1'd1 ; LSM_DETECT=1'd0; LSM_END=1'd0; CLK=1'd0;
 
 
 		end
 
 	initial
 		begin
-			$display("ENC  M1M0    muxA   muxE   muxD            ctlRregister                                                       IncReg         TIME");
-			$monitor("%d    %d     %d     %d     %d                %b         %d    %d", cu.encoder.OUT, cu.nextState.M1M0, cu.muxA.Y,cu.muxE.Y, cu.muxD.Y,cu.ctl_register.Q, cu.incrementerRegister.Q,$time);
+			$display("MA_OUT 	M1M0 	MR_OUT 	RESET 	ME_OUT 	MI 	INC_REG_OUT 	cu_datapath 	CLK 	TIME");
+			$monitor("%d %d %d %b %d %b %d %b %b %d",
+					cu_pepo.MA_OUT,
+					cu_pepo.M1M0,
+					cu_pepo.MR_OUT,
+					cu_pepo.RESET,
+					cu_pepo.ME_OUT,
+					cu_pepo.CTL_REG_CUI[19],
+					cu_pepo.INC_REG_OUT,
+					cu_datapath,
+					CLK,
+					$time);
 		end
 
-	always #20 CLK = ~CLK;
+	always #2 CLK = ~CLK;
 	initial #sim_time $finish;
 
-endmodule // mux4Tester
+endmodule // ControlUnit_Tester_pepo
 
 
--- IncRegister_pepo.v
+++ IncRegister_pepo.v
@@ -1,4 +1,4 @@
-module IncRegister_pepo (D, Enable, CLK, Q);
+module IncRegister_pepo (D, Enable, RESET, CLK, Q);
 
 	//Description
 		//Register placed at the output of the control unit adder to maintain the adder's value.
@@ -6,15 +6,19 @@ module IncRegister_pepo (D, Enable, CLK, Q);
 	//Inputs
 	input wire [7:0] D;		//Input data to the register
 	input wire Enable;		//Signals the register to latch or not
+	input wire RESET;
 	input wire CLK;
 
 	//Outputs
 	output reg [7:0] Q;		//Output data of the register.
 
 	//Implementation
-	always @ (posedge CLK)
-		if(Enable)
-			Q = D;
+	always @ (posedge CLK, RESET)
+		if(RESET)
+			Q = 8'd0;
+		else
+			if(Enable)
+				Q = D;
 endmodule // IncRegister
 
 /*
