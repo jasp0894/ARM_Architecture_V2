93acaade22ce90749bc16c347f07e8ea2920a865
Gilissa M. Matos <gilissa.matos@upr.edu>
2017-06-24 16:27:30 -0400

    Corrida sin #4

--- SLSManager.v
+++ SLSManager.v
@@ -1,44 +1,67 @@
-module SLSManager (output reg[2:0] OUT, input[31:0] IR, input SLS_EN);
+//Single Load/Store Manager
+//This module determines RAM setting for Addressing mode 2&3
+//The Output Bus organization is as follows:
+//OUT[3] -> SLS_R/W
+//OUT[2] -> Sign Extension 
+//OUT[1:0] -> Data Size
+//See ROM Table for more info.
+
+module SLSManager (output reg[3:0] OUT, input[31:0] IR, input SLS_EN);
+
+
 
 	always @ (IR, SLS_EN)
 
-	if(SLS_EN)
-	begin
-	// Addresing Mode 2
 
+
+	if(SLS_EN)
+
+	begin
+
+	// Addresing Mode 2
 		// Immediate & Register
 
 		if(IR[27:25] == 3'b010 || IR[27:25] == 3'b011)
-		begin					
 
+		begin					
 			if(IR[22] == 1'b1)	//post-indexed
-				OUT= 3'b000;					
+
+				OUT= 4'b0000;					
 			else 
-				OUT = 3'b010;  //unsigned word  (I don't know if  signed or unsigned. It's not clear from the documentation)
-			
-			
+				OUT = 4'b0010;  //unsigned word  (I don't know if  signed or unsigned. It's not clear from the documentation)
 		end	
+
 
 	//Addressing mode 3 
 
+
+
 		//Immediate & Register
 
+
+
 		if(IR[27:25] == 3'b000 && IR[4]==1'b1)
+
 		begin					
 
 			if(IR[20]==1'b1 && IR[5]==1'b0)	//LDRSB - Load Signed Byte 
-				OUT= 3'b100;					
+
+				OUT= 4'b0100;					
 			else if(IR[20]==1'b1 && IR[5]==1'b1)
-				OUT = 3'b101;  //LDRSH - Load Signed Halfword 
+
+				OUT = 4'b0101;  //LDRSH - Load Signed Halfword 
 			else
-				OUT = 3'b011;  //LDRD - Load Doubleword (don't know if signed of unsigned)
-				
-				
-			end	
+				OUT = 4'b0011;  //LDRD - Load Doubleword (don't know if signed of unsigned)
+		end	
+
+		if(IR[20] == 1'b1) //Load
+
+			OUT[3] = 1'b1;  //Read
+		else
+            OUT[3] = 1'b0; //Write
 
 
-		end
-	/*else 
-		OUT = 3'd0;*/
 
-endmodule // Encoder END
+	end
+
+endmodule // Encoder SLSManager
